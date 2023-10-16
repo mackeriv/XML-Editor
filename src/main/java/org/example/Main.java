26417650e9533aae80d5f9e8fa824a1e5581c1a5
mackeriv <mackeriv@gmail.com>
2023-10-16 15:08:10 -0300

    Quitting functionality.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,44 +17,56 @@ public class Main {
 
         System.out.println("Please provide the filepath for the XML file to be edited or enter \"Q\" to quit: ");
 
-        filepath = in.nextLine();
+        outerloop:
+        while (true) {
 
-        System.out.println("Choose \"1\" to change tag names or \"2\" to change tag contents: ");
+            filepath = in.nextLine();
 
-        try {
-            option = Integer.parseInt(in.nextLine());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Enter the name of the tag to be edited: ");
-
-        tagToChange = in.nextLine();
-
-        switch (option) {
-
-            case 1:
-                System.out.println("Enter a new name for the tag: ");
-                change = in.nextLine();
-
-                FileOps.docBuild();
-                ChangeOps.changeTagName();
-                FileOps.fileWriter();
+            //quits the application
+            if (filepath.equalsIgnoreCase("Q")) {
+                System.out.println("\nGoodbye!");
                 break;
+            }
 
-            case 2:
-                System.out.println("Enter the text to replace the contents of the tag: ");
-                change = in.nextLine();
+            System.out.println("Choose \"1\" to change tag names or \"2\" to change tag contents: ");
 
-                FileOps.docBuild();
-                ChangeOps.changeTagContents();
-                FileOps.fileWriter();
-                break;
+            // "input.nextInt()" can't be used in this context, so this is a workaround for parsing the input to int
+            // workaround: https://stackoverflow.com/a/13102066/22175138
+            try {
+                option = Integer.parseInt(in.nextLine());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
 
-            default:
-                System.out.println("ERROR");
-                break;
+            System.out.println("Enter the name of the tag to be edited: ");
 
+            tagToChange = in.nextLine();
+
+            switch (option) {
+
+                case 1:
+                    System.out.println("Enter a new name for the tag: ");
+                    change = in.nextLine();
+
+                    FileOps.docBuild();
+                    ChangeOps.changeTagName();
+                    FileOps.fileWriter();
+                    break outerloop;
+
+                case 2:
+                    System.out.println("Enter the text to replace the contents of the tag: ");
+                    change = in.nextLine();
+
+                    FileOps.docBuild();
+                    ChangeOps.changeTagContents();
+                    FileOps.fileWriter();
+                    break outerloop;
+
+                default:
+                    System.out.println("ERROR");
+                    break outerloop;
+
+            }
         }
     }
 }
